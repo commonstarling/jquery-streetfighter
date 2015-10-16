25250dfc336eacadd2301a444cf261ced3feeab4
commonstarling <sheenaramirez2@gmail.com>
2015-10-16 10:13:32 -0500

    attempted cool pose edits

--- js/app.js
+++ js/app.js
@@ -24,14 +24,12 @@ $(document).ready(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	})
-	.keydown(function(e) {
-	if(e.keyCode == 88) {
-		console.log('click');
-		/*$('.ryu-still').hide();
-		$('.ryu-cool').show();*/	
-		}
-	});
-});	
+	.keydown(function (e) {
+		if(e.keyCode == 88) {
+		$('.ryu-still').hide();
+		$('.ryu-cool').show() }	
+		});
+});
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
